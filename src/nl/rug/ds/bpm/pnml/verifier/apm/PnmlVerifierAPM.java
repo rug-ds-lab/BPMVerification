462594e8a00afaf07c1241b2982b87b8d1c86883
nrtpvanbeest <nrtpvanbeest@users.noreply.github.com>
2017-06-02 14:20:19 +1000

    Apromore version of PnmlVerifier

---
+++ src/nl/rug/ds/bpm/pnml/verifier/apm/PnmlVerifierAPM.java
@@ -0,0 +1,162 @@
+package nl.rug.ds.bpm.pnml.verifier.apm;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import hub.top.petrinet.PetriNet;
+import nl.rug.ds.bpm.event.EventHandler;
+import nl.rug.ds.bpm.event.VerificationEvent;
+import nl.rug.ds.bpm.event.VerificationLogEvent;
+import nl.rug.ds.bpm.event.listener.VerificationEventListener;
+import nl.rug.ds.bpm.event.listener.VerificationLogListener;
+import nl.rug.ds.bpm.pnml.verifier.ExtPnmlStepper;
+import nl.rug.ds.bpm.specification.jaxb.BPMSpecification;
+import nl.rug.ds.bpm.specification.marshaller.SpecificationUnmarshaller;
+import nl.rug.ds.bpm.specification.parser.SetParser;
+import nl.rug.ds.bpm.verification.Verifier;
+
+/**
+ * Created by Nick van Beest on 02-June-17.
+ */
+public class PnmlVerifierAPM implements VerificationEventListener, VerificationLogListener {
+	private EventHandler eventHandler;
+	private SetParser setParser;
+	private File nusmv2Binary;
+	private boolean reduce;
+	private PetriNet pn;
+
+	private String eventoutput;
+	private List<String> feedback;
+	private BPMSpecification bpmSpecification;
+	
+	public PnmlVerifierAPM(PetriNet pn, String specxml, String nusmv2) {
+		reduce = true;
+
+		//Make a shared eventHandler
+		eventHandler = new EventHandler();
+		setParser = new SetParser(eventHandler);
+
+		//Implement listeners and
+		//Add listeners to receive log and result notifications
+		eventHandler.addLogListener(this);
+		eventHandler.addEventListener(this);
+		
+		eventoutput = "";
+		feedback = new ArrayList<String>();
+		
+		this.nusmv2Binary = new File(nusmv2);
+		this.pn = pn;
+		
+		if (!(nusmv2Binary.exists() && nusmv2Binary.canExecute())) {
+			eventHandler.logCritical("No such file: " + nusmv2Binary.getPath());
+		}
+		
+		SpecificationUnmarshaller unmarshaller;
+		try {
+			unmarshaller = new SpecificationUnmarshaller(eventHandler, new ByteArrayInputStream(specxml.getBytes("UTF-8")));
+			bpmSpecification = unmarshaller.getSpecification();
+		} 
+		catch (UnsupportedEncodingException e) {
+			eventHandler.logCritical("Invalid specification xml");
+			return;
+		}
+		
+	}
+	
+	public List<String> verify() {
+		return verify(false);
+	}
+	
+	public List<String> verify(Boolean getAllOutput) {
+		//Make step class for specific Petri net type
+		ExtPnmlStepper stepper;
+		
+		try {
+			stepper = new ExtPnmlStepper(pn);
+			
+			//Make a verifier which uses that step class
+			Verifier verifier = new Verifier(stepper, eventHandler, nusmv2Binary);
+			//Start verification
+			verifier.verify(bpmSpecification, reduce);
+		} 
+		catch (Exception e) {
+			eventHandler.logCritical("Failed to load pnml");
+		}
+		System.out.println(eventoutput);
+		
+		return feedback;
+	}
+
+	public void addSpecification(String line) {
+		setParser.parse(line);
+	}
+
+	public void addSpecifications(String[] lines) {
+		for (String line: lines)
+			addSpecification(line);
+	}
+
+	public BPMSpecification getSpecifications() {
+		return setParser.getSpecification();
+	}
+	
+	public void setReduction(boolean reduce) {
+		this.reduce = reduce;
+	}
+
+	public boolean getReduction() {
+		return reduce;
+	}
+
+	//Set maximum amount of tokens at a single place
+	//Safety feature, prevents infinite models
+	//Standard value of 3
+	public void setMaximumTokensAtPlaces(int amount) {
+		Verifier.setMaximumTokensAtPlaces(amount);
+	}
+
+	public int getMaximumTokensAtPlaces() {
+		return Verifier.getMaximumTokensAtPlaces();
+	}
+
+	//Set maximum size of state space
+	//Safety feature, prevents memory issues
+	//Standard value of 7 million
+	//(equals models of 4 parallel branches with each 50 activities)
+	//Lower if on machine with limited memory
+	public void setMaximumStates(int amount) {
+		Verifier.setMaximumStates(amount);
+	}
+
+	public int getMaximumStates() {
+		return Verifier.getMaximumStates();
+	}
+
+	//Set log level VerificationLogEvent.DEBUG to VerificationLogEvent.CRITICAL
+	public void setLogLevel(int level) {
+		Verifier.setLogLevel(level);
+	}
+
+	public int getLogLevel() {
+		return Verifier.getLogLevel();
+	}
+
+	//Listener implementations
+	@Override
+	public void verificationEvent(VerificationEvent event) {
+		//Use for user feedback
+		//Event returns: specification id, formula, type, result, and specification itself
+		feedback.add(event.toString());
+	}
+	
+	@Override
+	public void verificationLogEvent(VerificationLogEvent event) {
+		//Use for log and textual user feedback
+		eventoutput += "[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "] " + event.toString() + "\n";
+	}
+}
