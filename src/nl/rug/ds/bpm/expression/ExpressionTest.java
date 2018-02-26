09e74e2eb7b7a347a8cf170eb71c89ff86f3aaff
nrtpvanbeest <nrtpvb@gmail.com>
2018-02-26 16:46:28 +1000

    nw test example

--- src/nl/rug/ds/bpm/expression/ExpressionTest.java
+++ src/nl/rug/ds/bpm/expression/ExpressionTest.java
@@ -6,8 +6,9 @@ public class ExpressionTest {
 		Expression<String> e = new Expression<String>("X", ExpressionType.EQ, "Bla");
 		Expression<String> e2 = new Expression<String>("X", ExpressionType.NEQ, "Bla");
 		
-		Expression<Integer> e3 = new Expression<Integer>("X", ExpressionType.EQ, 5);
-		Expression<Double> e4 = new Expression<Double>("X", ExpressionType.EQ, 5.0);
+		Expression<Double> e3 = new Expression<Double>("Y", ExpressionType.LT, 6.0);
+		Expression<Double> e4 = new Expression<Double>("Y", ExpressionType.EQ, 5.0);
+		Expression<Double> e5 = new Expression<Double>("Y", ExpressionType.NEQ, 4.0);
 		
 		System.out.println(e.accepts("Bo"));
 		System.out.println(e.accepts("Bla"));
@@ -17,12 +18,17 @@ public class ExpressionTest {
 		System.out.println(e2.accepts("Bla"));
 		System.out.println();
 		
-		System.out.println(e.contradicts(e2));		
+		System.out.println(e.contradicts(e3));		
 		System.out.println();
 		
 		System.out.println(e3.getValue());
 		System.out.println(e4.getValue());
 		System.out.println(e3.contradicts(e4));
+		System.out.println(e3.canContradict(e4));
+		
+		System.out.println();
+		System.out.println(e3.contradicts(e5));
+		System.out.println(e3.canContradict(e5));
 	}
 
 }
